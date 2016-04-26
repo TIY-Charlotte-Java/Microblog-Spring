39f052b6de58cb6461e7486c89c8772a9290c536
Michael Grill <Ultramar@michaels-mbp.apollosrc.root>
2016-04-26 15:24:21 -0400

    RealStuff

---
+++ MicroblogSpring/src/main/java/com/theironyard/clt/MicroblogSpringController.java
@@ -0,0 +1,18 @@
+package com.theironyard.clt;
+
+import com.sun.tools.internal.ws.processor.model.Model;
+
+
+public class MicroblogSpringController {
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String home(Model model, HttpSession session) {
+        model.addAttribute("name", session.getAttribute("userName"));
+        return "home";
+    }
+
+    @RequestMapping(path = "/login", method = RequestMethod.POST)
+    public String login(HttpSession session, String userName) {
+        session.setAttribute("userName", userName);
+        return "redirect:/";
+    }
+}
