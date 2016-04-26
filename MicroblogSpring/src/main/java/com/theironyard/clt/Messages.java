43b712f943e30d1efccf85ddffedaf4ac1911579
Michael Grill <Ultramar@michaels-mbp.apollosrc.root>
2016-04-26 16:18:58 -0400

    LastOfClassStuff

---
+++ MicroblogSpring/src/main/java/com/theironyard/clt/Messages.java
@@ -0,0 +1,9 @@
+package com.theironyard.clt;
+
+/**
+ * Created by Ultramar on 4/26/16.
+ */
+public class Messages {
+    int id;
+    String message;
+}
--- MicroblogSpring/src/main/java/com/theironyard/clt/MicroblogSpringController.java
+++ MicroblogSpring/src/main/java/com/theironyard/clt/MicroblogSpringController.java
@@ -5,9 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 
 
 public class MicroblogSpringController {
+
+    ArrayList<Messages> messages = new ArrayList<>();
+
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String home(Model model, HttpSession session) {
         model.addAttribute("name", session.getAttribute("userName"));
